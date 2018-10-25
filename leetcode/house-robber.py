06ebfa20b9146c375f3d4ec5447086bafed49fe7
Owen Sullivan <multiojuice@gmail.com>
2018-10-24 17:06:40 -0700

    Added some test cases

--- leetcode/house-robber.py
+++ leetcode/house-robber.py
@@ -10,21 +10,20 @@ def rob(nums):
 
 
     for index in range(len(nums) - 1, -1, -1):
-        print(index)
         if not index - 2 < 0:
-            print('\t', index-2)
             if dynamicMap[str(index)] + nums[index - 2] > dynamicMap[str(index - 2)]:
                 dynamicMap[str(index - 2)] = dynamicMap[str(index)] + nums[index - 2]
 
         if not index - 3 < 0:
-            print('\t', index-3)
             if dynamicMap[str(index)] + nums[index - 3] > dynamicMap[str(index - 3)]:
                 dynamicMap[str(index - 3)] = dynamicMap[str(index)] + nums[index - 3]
 
 
-    print(dynamicMap)
     return max(dynamicMap['0'], dynamicMap['1'])
 
 
+print(rob([500, 4 , 100 , 200 , 101]))
+print(rob([500, 4 , 100 , 200 , 50]))
+print(rob([1,2,3,1]))
 print(rob([2,7,9,3,1]))
 
