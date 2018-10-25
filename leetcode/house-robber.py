23e93bc92a6df608d67afb525122d2e1fb767972
Owen Sullivan <multiojuice@gmail.com>
2018-10-24 17:03:33 -0700

    Finished that problem

--- leetcode/house-robber.py
+++ leetcode/house-robber.py
@@ -1,12 +1,30 @@
+dynamicMap = {}
+
 def rob(nums):
-    total1 = 0
-    for index in range(0, len(nums), 2):
-        total1 += nums[index]
-    total2 = 0
-    for index in range(1, len(nums), 2):
-        total2 += nums[index]
 
-    return max(total1,total2)
+    for index in range(0, len(nums)):
+        dynamicMap[str(index)] = 0
 
-print(rob([]))
+    dynamicMap[str(len(nums) - 1)] = nums[-1]
+    dynamicMap[str(len(nums) - 2)] = nums[-2]
+
+
+    for index in range(len(nums) - 1, -1, -1):
+        print(index)
+        if not index - 2 < 0:
+            print('\t', index-2)
+            if dynamicMap[str(index)] + nums[index - 2] > dynamicMap[str(index - 2)]:
+                dynamicMap[str(index - 2)] = dynamicMap[str(index)] + nums[index - 2]
+
+        if not index - 3 < 0:
+            print('\t', index-3)
+            if dynamicMap[str(index)] + nums[index - 3] > dynamicMap[str(index - 3)]:
+                dynamicMap[str(index - 3)] = dynamicMap[str(index)] + nums[index - 3]
+
+
+    print(dynamicMap)
+    return max(dynamicMap['0'], dynamicMap['1'])
+
+
+print(rob([2,7,9,3,1]))
 
