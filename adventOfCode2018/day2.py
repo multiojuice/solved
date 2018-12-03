3a0c451f9c77f729a67e452b371e2184d95b8078
Owen J Sullivan <multiojuice@gmail.com>
2018-12-02 20:57:33 -0800

    did day two part two in a hacky way. will fix

--- adventOfCode2018/day2.py
+++ adventOfCode2018/day2.py
@@ -30,5 +30,29 @@ def scanIds(fileName):
   
   return twoLettered * threeLettered
 
+def findSimilar(fileName):
+  stringList = []
+  with open(fileName) as file:
+    # go through each line in the file
+    for line in file:
+      stringList.append(line)
+
+  for string1 in stringList:
+    for string2 in stringList:
+      oneDiff = False
+      similar = True
+      for index in range(len(string1)):
+        if string1[index] != string2[index]:
+          if not oneDiff:
+            oneDiff = True
+          else:
+            similar = False
+            break
+        
+      if similar and oneDiff:
+        return string1, string2
+
+      
 
 print(scanIds('data/day2.txt'))
+print(findSimilar('data/day2.txt'))
