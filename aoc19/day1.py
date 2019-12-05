1d04c6f898f39e18b2c255ba26607d85a7ff0b98
Owen Sullivan <multiojuice@gmail.com>
2019-12-05 11:09:47 -0500

    Completed Day1 AOC2019

--- aoc19/day1.py
+++ aoc19/day1.py
@@ -3,13 +3,22 @@ import math
 def calcFuel(mass):
     fuel = mass / 3
     fuel = math.floor(fuel)
-    return fuel - 2
+    fuel = fuel - 2
+
+    if fuel > 0:
+        return fuel
+    return 0
 
 
 total = 0
 with open("input1") as file:
     for line in file:
         current_mass = int(line)
-        total += calcFuel(current_mass)
+
+        fuel = calcFuel(current_mass)
+        total += fuel
+        while fuel is not 0:
+            fuel = calcFuel(fuel)
+            total += fuel
 
 print(total)
