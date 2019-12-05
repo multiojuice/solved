29f2a0a228490e1d097e44e6906b127a49d96f9a
Owen Sullivan <multiojuice@gmail.com>
2019-12-05 11:35:42 -0500

    Pt2 D2 AOC19

--- aoc19/day2.py
+++ aoc19/day2.py
@@ -1,29 +1,40 @@
 st = input()
 
-positions = st.split(',')
+myinput = st.split(',')
 
-for i in range(0, len(positions)):
-    positions[i] = int(positions[i])
-
-
-positions[2] = 2
-positions[1] = 12
+for i in range(0, len(myinput)):
+    myinput[i] = int(myinput[i])
 
 error = False
-for i in range(0, len(positions), 4):
-    opcode = positions[i]
-    if opcode == 99:
-        break
-    elif opcode == 1:
-        arg1 = positions[positions[i+1]]
-        arg2 = positions[positions[i+2]]
-        positions[positions[i+3]] = arg1 + arg2
-    elif opcode == 2:
-        arg1 = positions[positions[i + 1]]
-        arg2 = positions[positions[i + 2]]
-        positions[positions[i + 3]] = arg1 * arg2
-    else:
-        error = True
-        break
+answer = False
+for noun in range (0, 100):
+    for verb in range(0, 100):
+        """
+        How naive
+        """
+        positions = list.copy(myinput)
+        positions[2] = verb
+        positions[1] = noun
+        for i in range(0, len(positions), 4):
+            opcode = positions[i]
+            if opcode == 99:
+                if positions[0] == 19690720:
+                    answer = True
+                    print(100 * noun + verb)
+                    break
+            elif opcode == 1:
+                arg1 = positions[positions[i+1]]
+                arg2 = positions[positions[i+2]]
+                positions[positions[i+3]] = arg1 + arg2
+            elif opcode == 2:
+                arg1 = positions[positions[i + 1]]
+                arg2 = positions[positions[i + 2]]
+                positions[positions[i + 3]] = arg1 * arg2
+            else:
+                error = True
+                break
 
-print(positions)
+        if answer:
+            break
+    if answer:
+        break
